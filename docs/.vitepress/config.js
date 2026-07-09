4963edc19ccb7925bd3dbe29d41c72efbdca2564
Cheng0720 <3281814282@qq.com>
2026-07-09 21:50:12 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+```javascript
+export default {
+  title: '程子为的博客',
+  description: '个人技术学习记录',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文章', link: '/posts/hello-world' }
+    ]
+  }
+}
+```
